205565de6ba8edcb1b53d56e49d9ad6c5c0f8b2d
sagalaev mark <sagalaev.m@gmail.com>
2024-09-09 21:57:30 +0400

    upd choice location querySelector

--- index.js
+++ index.js
@@ -12,19 +12,32 @@ let scrape = async () => {
     await page.setViewport({width: 1080, height: 1024});
     await page.screenshot({path: 'screenshot.jpg'});
 
-    await page.waitForSelector('.Region_region__6OUBn');
-    await page.locator('.Region_region__6OUBn').click();
-    await page.waitForSelector('.UiRegionListBase_list__cH0fK');
-    const locationsElementHandles = await page.$eval('.UiRegionListBase_list__cH0fK', (el, LOCATION) => {
-        el.querySelector("li[title=\"" + LOCATION + "\"]");
-    }, LOCATION);
-    console.log(locationsElementHandles)
-    // locationsElementHandles.
-    // // .UiRegionListBase_list__cH0fK'
-    // let qwerty = await text?.evaluate(el => el.querySelector("li[title=" + LOCATION + "]"));
-    // console.log(qwerty);
-    // console.log(qwe)
-    // await (await page.$(`[title="${LOCATION}"]`)).click();
+    if (await page.waitForSelector('.Region_region__6OUBn')) {
+        await page.locator('.Region_region__6OUBn').click();
+    }
+    if (await page.waitForSelector('.UiRegionListBase_list__cH0fK')) {
+        let li = await page.$eval('li["Санкт-Петербург и область"]', (el, LOCATION) =>
+            el.textContent
+        );
+        console.log(li);
+        // let li = await page.$eval('.UiRegionListBase_list__cH0fK', (el, LOCATION) => {
+        //     let lis = el.querySelectorAll("li")
+        //     for (const liKey in lis) {
+        //         let li = lis[liKey]
+        //         if (li.textContent?.includes(LOCATION)) {
+        //             return li.waitHandle();
+        //         }
+        //     }
+        // }, LOCATION);
+        // console.log(li);
+        // li.click();
+    }
+
+    if (await page.waitForSelector('.Region_region__6OUBn')) {
+        let qwer = await page.$eval('.Region_region__6OUBn', (el) => el.textContent);
+        console.log(qwer)
+    }
+
     const textSelector = await page.locator('.Price_price__QzA8L').waitHandle();
     const fullTitle = await textSelector?.evaluate(el => el.textContent);
     console.log(fullTitle);
