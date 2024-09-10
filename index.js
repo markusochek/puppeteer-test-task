c95ed3436b940eb09f2f342c0e9df152fc6da058
Sagalaev Mark <sagalaev.bot@gmail.com>
2024-09-10 16:17:33 +0400

    add complete choice location, prices, rating, reviewCount

--- index.js
+++ index.js
@@ -1,49 +1,80 @@
 const puppeteer = require('puppeteer');
+const {writeFile, appendFile} = require("node:fs");
 
-const URL = process.argv[2];
-const LOCATION = process.argv[3];
+const CLASS_NAME_LOCATION_CHOICE = '.Region_region__6OUBn';
+const CLASS_NAME_PRICE = '.PriceInfo_root__GX9Xp';
+const CLASS_NAME_PRODUCT_RATING = '.ActionsRow_stars__EKt42';
+const CLASS_NAME_PRODUCT_REVIEW_COUNT = '.ActionsRow_reviews__AfSj_';
+
+const PRODUCT_FILE_NAME = "product.txt";
+writeFile(PRODUCT_FILE_NAME, "", (err) => {
+    if (err) {
+        console.error(err);
+    }
+});
+
+const url = process.argv[2];
+const location = process.argv[3];
 
 let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-size=1920, 1080'],
+    });
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+    await page.goto(url, {waitUntil: "domcontentloaded"});
 
-    await page.goto(URL);
-
-    await page.setViewport({width: 1080, height: 1024});
+    // возможно костыль на задержку,
+    // но почитав мануал и комментарии к проблеме с начальной многократной перезагрузкой страницы,
+    // не нашел другого 100% способа получения данных
+    await sleep(5000);
+    await clickByClassName(page, CLASS_NAME_LOCATION_CHOICE);
+    await clickByText(page, location);
+    await sleep(2000);
+    await scraping(page, CLASS_NAME_PRICE).then((parsingString) => {
+        let [priceOld, price] = parsingString.split(' ').filter(item => !isNaN(parseFloat(item.replace(',', '.'))));
+        appendTextFile(PRODUCT_FILE_NAME, `price=${price}\n`);
+        appendTextFile(PRODUCT_FILE_NAME, `priceOld=${priceOld}\n`);
+    });
+    await scraping(page, CLASS_NAME_PRODUCT_RATING).then((rating) => {
+        appendTextFile(PRODUCT_FILE_NAME, `rating=${rating}\n`);
+    });
+    await scraping(page, CLASS_NAME_PRODUCT_REVIEW_COUNT).then((reviewCountAndSymbol) => {
+        let reviewCount = reviewCountAndSymbol.split(' ')[0];
+        appendTextFile(PRODUCT_FILE_NAME, `reviewCount=${reviewCount}`);
+    });
     await page.screenshot({path: 'screenshot.jpg'});
-
-    if (await page.waitForSelector('.Region_region__6OUBn')) {
-        await page.locator('.Region_region__6OUBn').click();
-    }
-    if (await page.waitForSelector('.UiRegionListBase_list__cH0fK')) {
-        let li = await page.$eval('li["Санкт-Петербург и область"]', (el, LOCATION) =>
-            el.textContent
-        );
-        console.log(li);
-        // let li = await page.$eval('.UiRegionListBase_list__cH0fK', (el, LOCATION) => {
-        //     let lis = el.querySelectorAll("li")
-        //     for (const liKey in lis) {
-        //         let li = lis[liKey]
-        //         if (li.textContent?.includes(LOCATION)) {
-        //             return li.waitHandle();
-        //         }
-        //     }
-        // }, LOCATION);
-        // console.log(li);
-        // li.click();
-    }
-
-    if (await page.waitForSelector('.Region_region__6OUBn')) {
-        let qwer = await page.$eval('.Region_region__6OUBn', (el) => el.textContent);
-        console.log(qwer)
-    }
-
-    const textSelector = await page.locator('.Price_price__QzA8L').waitHandle();
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-    console.log(fullTitle);
 
     await browser.close();
 };
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function clickByClassName(page, classNameLocationChoice) {
+    let el = await page.waitForSelector(classNameLocationChoice);
+    await el.click();
+}
+
+async function clickByText(page, text) {
+    let el = await page.waitForSelector(`text/${text}`);
+    await el.click();
+}
+
+async function scraping(page, className) {
+    return await page.$eval(className, el => el.textContent);
+}
+
+function appendTextFile(fileName, output) {
+    appendFile(fileName, output, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
 
 scrape().then(() => {
 });
